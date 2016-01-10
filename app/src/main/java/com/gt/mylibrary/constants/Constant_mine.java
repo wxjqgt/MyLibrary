de1b65f3b56fa132b8fc9515893273109131a043
wxjqgt <wwwmcom1905>
2016-01-10 22:15:13 +0800

    volleyUtils修改

---
+++ app/src/main/java/com/gt/mylibrary/constants/Constant_mine.java
@@ -0,0 +1,18 @@
+package com.gt.mylibrary.constants;
+
+/**
+ * Created by wxjqgt on 2015/12/18.
+ */
+public class Constant_mine {
+    public static final String MUSICS = "musics";
+    public static final String MOVIES = "movies";
+    public static final String IMAGES = "images";
+    public static final String DOCUMENT = "document";
+    public static final int SDSTORAGE = 1;
+    public static final int INTERNALSTORAGE = 2;
+    public static final String URL = "http://www.baidu.com";
+    public static final String FIRST_PAGE_WEBVIEW = "http://ikft.house.qq.com/index.php?" +
+            "guid=866500021200250&devua=appkft_1080_1920_XiaomiMI4LTE_1.8.3_Android19&" +
+            "devid=866500021200250&appname=QQHouse&mod=appkft&act=homepage&channel=71" +
+            "&cityid=%s";
+}
